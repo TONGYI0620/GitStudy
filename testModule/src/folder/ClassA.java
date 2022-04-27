26efe7d389c8511cc895a12cd9d59a759471ee4e
TongYi <1461440620@qq.com>
2022-04-27 20:31:09 +0800

    dev A commit1

---
+++ testModule/src/folder/ClassA.java
@@ -0,0 +1,18 @@
+package folder;
+
+
+import org.junit.Test;
+
+/**
+ * Description:
+ * date: 2022/4/27 20:16
+ *
+ * @author 童一
+ */
+public class ClassA {
+
+    @Test
+    public void methodA () {
+        System.out.println("This is devA ClassA.methodA ");
+    }
+}
