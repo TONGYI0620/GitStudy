89e5686c9281b46037d1bff6494418a517ed8fc5
TongYi <1461440620@qq.com>
2022-04-27 20:21:41 +0800

    commit4

---
+++ testModule/src/folder/ClassM.java
@@ -0,0 +1,17 @@
+package folder;
+
+import org.junit.Test;
+
+/**
+ * Description:
+ * date: 2022/4/27 20:19
+ *
+ * @author 童一
+ */
+public class ClassM {
+
+    @Test
+    public void methodM () {
+        System.out.println("This is devA ClassM.methodM ");
+    }
+}
