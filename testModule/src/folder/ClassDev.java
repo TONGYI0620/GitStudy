aef7593ce2e2d2483f6bccab18644114f0f2675c
TongYi <1461440620@qq.com>
2022-05-07 15:58:54 +0800

    dev commit 1

---
+++ testModule/src/folder/ClassDev.java
@@ -0,0 +1,17 @@
+package folder;
+
+import org.junit.Test;
+
+/**
+ * Description:
+ * date: 2022/5/7 15:55
+ *
+ * @author 童一
+ */
+public class ClassDev {
+
+    @Test
+    public void methodM () {
+        System.out.println("This is dev branch");
+    }
+}
